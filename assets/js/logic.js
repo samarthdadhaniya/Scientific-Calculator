ddc5742cfecaa93701fd2ef929bf0a689c1a6633
samarth <samarth.d@simformsolutions.com>
2023-03-02 12:15:13 +0530

    :label: Selecting Elements with it's id and store into specific variable

---
+++ assets/js/logic.js
@@ -0,0 +1,70 @@
+const display = document.querySelector("#input_display");
+
+// getting number by id------------------------------------
+
+const zero = document.querySelector("#zero_Number");
+const one = document.querySelector("#one_Number");
+const two = document.querySelector("#two_Number");
+const three = document.querySelector("#three_Number");
+const four = document.querySelector("#four_Number");
+const five = document.querySelector("#five_Number");
+const six = document.querySelector("#six_Number");
+const seven = document.querySelector("#seven_Number");
+const eight = document.querySelector("#eight_Number");
+const nine = document.querySelector("#nine_Number");
+const dot = document.querySelector("#dot_operator");
+// ---------------------------------------------------------
+
+// getting basic operator select by id ----------------------------
+const addition_Operator = document.querySelector('#addition_sign');
+const subtraction_Operator = document.querySelector('#subtraction_sign');
+const multiplication_Operator = document.querySelector('#multiplication_sign');
+const division_Operator = document.querySelector('#division_sign');
+const modulus_Operator = document.querySelector('#modulus_sign');
+const equal_To = document.querySelector('#equalto_sign');
+// -----------------------------------------------------------------
+
+// Memory Function Seclecting with ID-------------------------
+const MemoryClearBtn = document.querySelector("#MemoryClear")
+const MemoryReadBtn = document.querySelector("#MemoryRead")
+const MemoryPluseBtn = document.querySelector("#MemoryPluse")
+const MemoryMinusBtn = document.querySelector("#MemoryMinus")
+const MemoryStoreBtn = document.querySelector("#MemoryStore")
+// ------------------------------------------------------------
+
+// all common function select with id------------------------
+const floorbtn = document.querySelector("#floor");
+const ceilbtn = document.querySelector("#ceil");
+const randombtn = document.querySelector("#random");
+const clearScreen = document.querySelector('#clear');
+const backEraseButton = document.querySelector('#backEraseButton');
+const logbtn = document.querySelector('#logbtn');
+const lnbtn = document.querySelector('#lnbtn');
+const xsqurebtn = document.querySelector('#xsqurebtn');
+const squarerootbtn = document.querySelector('#squareroot');
+const tenpowerxbtn = document.querySelector('#tenpowerx');
+const oneuponxbtn = document.querySelector('#oneuponx');
+const modxbtn = document.querySelector('#modx');
+const expbtn = document.querySelector('#exp');
+const fectorialbtn = document.querySelector('#fectorial');
+const pibtn = document.querySelector('#pi');
+const constantEbtn = document.querySelector('#constantE');
+const degreebtn = document.querySelector('#degree')
+const radianbtn = document.querySelector('#radian')
+const FEbtn = document.querySelector('#FE')
+const leftParenthesisbtn = document.querySelector('#leftParenthesis')
+const rightParenthesisbtn = document.querySelector('#rightParenthesis')
+const exponentbtn = document.querySelector('#exponent')
+const xcudebtn = document.querySelector('#xcube');
+const cuberootbtn = document.querySelector('#cuberoot')
+const expmonebtn = document.querySelector('#expmone');
+const tworestooxbtn = document.querySelector('#tworestoox');
+const logonepbtn = document.querySelector('#logonep');
+const eraisedtooxbtn = document.querySelector('#eraisedtoox');
+
+const sinbtn = document.querySelector('#sin');
+const secbtn = document.querySelector('#sec');
+const cosbtn = document.querySelector('#cos');
+const cscbtn = document.querySelector('#csc');
+const tanbtn = document.querySelector('#tan');
+const cotbtn = document.querySelector('#cot');
